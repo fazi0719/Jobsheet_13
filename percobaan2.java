4fd21e21d3123dee3f21e27978617964c29125af
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-12-19 16:33:10 +0700

    soal no 2

--- percobaan2.java
+++ percobaan2.java
@@ -8,13 +8,22 @@ public class percobaan2 {
             return (x * hitungPangkat(x, y - 1));
         }
     }
+     static void cetakDeret (int x, int y) {
+         for (int i = 0; i < y; i++) {
+            System.out.print(x + "x");
+        }
+        System.out.print("1"); 
+    }
 public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int bilangan, pangkat;
         System.out.print(" Bilangan yang dihitung: ");
-       bilangan = sc.nextInt();
+        bilangan = sc.nextInt();
         System.out.print(" Pangkat: ");
         pangkat= sc.nextInt();
+        System.out.print("Proses: ");
+        cetakDeret(bilangan, pangkat);
+        System.out.print(" = ");
         System.out.println(hitungPangkat(bilangan, pangkat));
         sc.close();
 }
